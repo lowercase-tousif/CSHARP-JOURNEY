95085765cea6c78ee623cf28675a289c085b86c7
lowercase-tousif <toisiftasrik@gmail.com>
2025-01-18 22:24:55 +0600

    Learned Inheritance

---
+++ MasteringDesignPatterns/Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using Inheritance;
+
+var car = new Car();
+
+car.Brand = "Toyota";
+car.Model = "Corolla";
+car.Year = 2021;
+car.NumberOfDoors = 4;
+car.NumberOfWheels = 4;
+
+car.Start();
+car.Stop();
+
+var bike = new Bike();
+
+bike.Brand = "Giant";
+bike.Model = "Defy";
+bike.Year = 2021;
+bike.NumberOfWheels = 2;
+
+bike.Start();
+bike.Stop();
---
+++ MasteringDesignPatterns/Inheritance/Vehicle.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("Starting the vehicle...");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopping the vehicle...");
+        }
+    }
+}
